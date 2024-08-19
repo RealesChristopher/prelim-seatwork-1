63df7e94e8a8faf37c8170279dd396fef1d08b7f
Christopher Reales <95603-18531537@SERVER2.DOMAIN>
2024-08-19 18:25:06 +0800

    First Commit

---
+++ Index.js
@@ -0,0 +1,13 @@
+const user = {
+    fname: "Jessica",
+    lname: "Cordero",
+    gender: "Female",
+    age: 30
+}
+
+const fname = user.fname
+const lname = user.lname
+const gender = user.gender
+const age = user.age
+
+console.log("I am" + " " + fname + " " + lname + " and I am " + age + " years old")
